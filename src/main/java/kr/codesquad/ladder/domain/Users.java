9eefe8b6e02876e85b60608244bb18066165ed2e
saehim <saeilee.vxv@gmail.com>
2023-03-09 16:22:45 +0900

    Refactor (Users) : `isValidUserNum` 유효성 검사 메서드 이동
    - 좀 더 로직에 어울리는 클래스로 이동 `Ladder` ->  `Users`로 이동

--- src/main/java/kr/codesquad/ladder/domain/Users.java
+++ src/main/java/kr/codesquad/ladder/domain/Users.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 
 public class Users {
     private List<User> userList;
+    private final int MAX_USER_NUM = 100;
+    private final int MIN_USER_NUM = 1;
 
     public Users(List<String> nameList) {
         this.userList = generateUserList(nameList);
@@ -12,6 +14,8 @@ public class Users {
 
     // 중복 이름 제거
     private List<User> generateUserList(List<String> nameList) {
+        isValidUserNum(nameList.size());
+
         return nameList.stream().distinct()
                 .map(User::new)
                 .collect(Collectors.toList());
@@ -26,5 +30,10 @@ public class Users {
      * */
     public String getUserName(int order) {
         return userList.get(order).getName();
+    }
+    private void isValidUserNum (int userNum) {
+        if (userNum > MAX_USER_NUM || userNum < MIN_USER_NUM) {
+            throw new IllegalArgumentException(String.format("[exception] 참여자 수는 %d~%d 범위 내여야 합니다.", MIN_USER_NUM, MAX_USER_NUM));
+        }
     }
 }
