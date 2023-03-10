eb38a163020273eaf60148f164e22b1602cf938c
saehim <saeilee.vxv@gmail.com>
2023-03-10 15:28:23 +0900

    Feat (InputView) : 실행 결과를 입력받는 메서드 구현
    - 사다리 게임의 결과를 입력받는 메서드 구현
    - 사용자에 대한 메서드 이름을 통일했다. `person` -> `user`

--- src/main/java/kr/codesquad/ladder/view/InputView.java
+++ src/main/java/kr/codesquad/ladder/view/InputView.java
@@ -8,12 +8,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class InputView {
-    private final String GET_PERSON_NAME_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요.)";
+    private final String GET_USER_NAME_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요.)";
     private final String GET_STEP_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+    private final String GET_RESULTS_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private final String REQUEST_GAME_RESULT = "결과를 보고 싶은 사람은?";
+
     private final String TAG_EXCEPTION = "[exception] ";
     private final String NUMBER_FORMAT_ERROR_MESSAGE = "잘못된 숫자 형식입니다.";
     private final String SMALL_NUMBER_ERROR_MESSAGE = "너무 작은 숫자입니다. (0 초과한 숫자 입력)";
-    private final String BIG_NUMBER_ERROR_MESSAGE = "너무 큰 숫자입니다. (100 이하 숫자 입력)";
     private final String REG = ",";
     private BufferedReader bufferedReader;
 
@@ -21,8 +23,20 @@ public class InputView {
         bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     }
 
+    public String getUserNameForGameResult() throws IOException {
+        System.out.println(System.lineSeparator() + REQUEST_GAME_RESULT);
+        return getValidInput().trim();
+    }
+
+    public List<String> getResults() throws IOException {
+        System.out.println(GET_RESULTS_MESSAGE);
+        String[] resluts = getValidInput().split(REG);
+
+        return Arrays.stream(resluts).map(String::trim).collect(Collectors.toList());
+    }
+
     public List<String> getUserNames() throws IOException {
-        System.out.println(GET_PERSON_NAME_MESSAGE);
+        System.out.println(GET_USER_NAME_MESSAGE);
         String[] names = getValidInput().split(REG);
 
         return Arrays.stream(names).map(String::trim).collect(Collectors.toList());
