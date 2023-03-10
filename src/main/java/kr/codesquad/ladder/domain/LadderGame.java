ee008c79d50eec63236a03763dfdcc08d2f6dc39
saehim <saeilee.vxv@gmail.com>
2023-03-10 15:46:38 +0900

    Feat (LadderGame) : 유저 이름 입력시 결과값 반환 기능 구현
    - 유저 이름 입력시 결과값 반환 기능 구현
    - 사다리 게임의 결과 생성 메서드
    - 이름 입력시 결과 반환 메서드
    - 리팩토링이 필요해 보인다.

--- src/main/java/kr/codesquad/ladder/domain/LadderGame.java
+++ src/main/java/kr/codesquad/ladder/domain/LadderGame.java
@@ -1,14 +1,19 @@
 package kr.codesquad.ladder.domain;
 
+import kr.codesquad.ladder.dto.GameResult;
 import kr.codesquad.ladder.view.InputView;
 import kr.codesquad.ladder.view.OutputView;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.NoSuchElementException;
 
 public class LadderGame {
     private Ladder ladder;
     private Users users;
+    private List<GameResult> gameResultList;
     private InputView inputView;
     private OutputView outputView;
 
@@ -20,14 +25,49 @@ public class LadderGame {
     public void run() {
         try {
             users = new Users(inputView.getUserNames());
-            int lineNum = inputView.getStepNum();
+            List<String> expectedResult = inputView.getResults();
+            isValidGameData(users.size(), expectedResult.size());
 
-            ladder = new Ladder(users.size(), lineNum);
+            ladder = new Ladder(users.size(), inputView.getStepNum());
+            gameResultList = generateGameResult(users.getAllUserNames(), expectedResult);
 
-            outputView.printLadder(ladder, users);
+            outputView.printLadder(ladder, users.getAllUserNames(), expectedResult);
+
+
+            while (true) {
+                String userName = inputView.getUserNameForGameResult();
+                String result = getUsersGameResult(userName);
+
+               outputView.print(result);
+            }
 
         } catch (IOException | IllegalArgumentException e) {
             System.out.println(e.getMessage());
+        } catch (NoSuchElementException e) {
+            System.out.println("게임을 종료합니다.");
+        }
+    }
+
+    private String getUsersGameResult(String userName) throws NoSuchElementException {
+        if (userName.toLowerCase(Locale.ROOT).equals("all")) {
+            return 
+        }
+        return gameResultList.stream()
+                .filter(e -> e.isUserData(userName))
+                .map(GameResult::getGameResult)
+                .findFirst().get();
+    }
+
+    private List<GameResult> generateGameResult(List<String> users, List<String> gameResults) {
+        List<GameResult> gameResultList = new ArrayList<>();
+
+        for (int i=0; i<users.size(); i++) {
+            int endPoint = ladder.getEndPoint(i);
+            gameResultList.add(new GameResult(users.get(i), gameResults.get(endPoint)));
+        }
+
+        return gameResultList;
+    }
         }
     }
 
